032058cc66e55481761d79588dd685984920ca37
Mehedi Hasan <mehedihasan634st@gmail.com>
2026-01-23 15:58:56 +0600

    const1-Double_Rent

---
+++ Double_Rent.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int initialRent, finalRent;
+    cin >> initialRent;
+
+    finalRent = 2 * initialRent;
+
+    cout << finalRent << endl;
+    return 0;
+}
