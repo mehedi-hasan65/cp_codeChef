88857ba0b8e68e0202f20b171abf1a70c03bde32
Mehedi Hasan <mehedihasan634st@gmail.com>
2026-02-05 22:35:23 +0600

    c3-Ageing

---
+++ Ageing.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        int y = x - 10;
+        cout << y << endl;
+    }
+    return 0;
+}
