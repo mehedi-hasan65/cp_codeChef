641cf83ab81eb35fa534ca86f7fd697fa98ec966
Mehedi Hasan <mehedihasan634st@gmail.com>
2026-02-07 00:38:40 +0600

    -F_F

---
+++ F_F.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "L";
+    for (int i = 1; i <= n; i++)
+    {
+
+        cout << "o";
+    }
+    cout << "ng" << endl;
+    return 0;
+}
